8f29d1122d55d02390532f0d499b8d1ad695ed19
Katie Hoffman <ktahoffman@gmail.com>
2015-04-01 12:46:11 -0400

    add solution

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140922151507) do
+ActiveRecord::Schema.define(version: 20150401150815) do
 
   create_table "frogs", force: :cascade do |t|
     t.string  "name"
@@ -20,8 +20,9 @@ ActiveRecord::Schema.define(version: 20140922151507) do
   end
 
   create_table "ponds", force: :cascade do |t|
-    t.string "name"
     t.string "water_type"
+    t.string "name"
+    t.string "color"
   end
 
   create_table "tadpoles", force: :cascade do |t|
